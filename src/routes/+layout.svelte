0274e41411d7fe20e91358ea25e028c32de165d3
rskrzydelski <r.skrzydelski@gmail.com>
2023-08-16 13:28:53 +0200

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,10 @@
 <script>
+  // vercel analytics
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+
+  inject({ mode: dev ? 'development' : 'production' });
+
   // Your selected Skeleton theme:
   import '@skeletonlabs/skeleton/themes/theme-gold-nouveau.css';
 
